beb3dbe247f5381607e472fa6e1f42ac73745968
Iliya-Tsvibel <45923437+Iliya-Tsvibel@users.noreply.github.com>
2022-05-23 19:03:14 +0300

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,21 +1,38 @@
-def is_pal(a)
+# 1.
+#Read about Big O notation: https://en.wikipedia.org/wiki/Big_O_notation
+#This first solution optimized for CPU, unlike subsequent additional solutions.
+def is_pal(a):
     for n in range(len(a) // 2):
         if a[n] != a[len(a) - n - 1]:
             return False
     return True
 
-is_pal("abcba")
-
-
-# 1.
+res = is_pal("abcba")
+print (res)
+####################
+# 2.
+def isPalindrome(s):
+    rev = ''.join(reversed(s))
+    if (s == rev):
+        return True
+    return False
+s = "malayalam"
+ans = isPalindrome(s)
+ 
+if (ans):
+    print("Yes")
+else:
+    print("No")
+####################
+# 3.
 a = "abcba"
-c = a[::-1]
-if a == c:
+b = a[::-1]
+if a == b:
     print("the word", a, "is palindrome")
 else:
     print("the word", a, "is not palindrome")
 ####################
-# 2. 
+# 4. 
 x = "maLayalam"
 y = x.lower()
 w = ""
@@ -27,7 +44,7 @@ if (y == w):
 else:
     print("No")
 ####################
-# 3.
+# 5.
 def checkPalindrome(s):
     return list(s) == list(reversed(s))
 

fa605f603496b3e27079703bf0abfc838b8a6744
Iliya-Tsvibel <45923437+Iliya-Tsvibel@users.noreply.github.com>
2022-05-31 18:47:25 +0300

    Update Balanced_brackets.py

--- Balanced_brackets.py
+++ Balanced_brackets.py
@@ -1,3 +1,5 @@
+#Time complexity is O(n) where the n is total number of characters
+#Using while
 class Solution:
    def isValid( self, sequence ):
        while True:
@@ -15,3 +17,58 @@ if __name__ == '__main__':
    print(f'Is {sequence1} valid ? : {Solution().isValid(sequence1)}')
    sequence2 = input("Enter a string of brackets:")
    print(f'Is {sequence2} valid ? : {Solution().isValid (sequence2)}')
+######################################################################
+#Using stack
+class Solution:
+   def isValid (self, sequence: str):
+       stack = []
+       opening = set('([{')
+       closing = set(')]}')
+       pair = {')' : '(' , ']' : '[' , '}' : '{'}
+       for i in sequence :
+           if i in opening :
+               stack.append(i)
+           if i in closing :
+               if not stack :
+                   return False
+               elif stack.pop() != pair[i] :
+                   return False
+               else :
+                   continue
+       if not stack :
+           return True
+       else :
+           return False
+
+if __name__ == "__main__":
+    sequence1 = input("Enter a string of brackets:")
+    print(f"Is {sequence1} valid ? : {Solution().isValid(sequence1)}")
+    sequence2 = input("Enter a string of brackets:")
+    print(f"Is {sequence2} valid ? : {Solution().isValid (sequence2)}")
+######################################################################
+#Using stack2
+def BalancedBrackets(Str):
+    stack = []
+    for char in Str:
+        if char == '{' or char == '(' or char == '[':
+            stack.append(char) # push
+        elif char == '}' or char == ')' or char == ']':
+            if len(stack) == 0:
+                return False
+            top_element = stack.pop() # pop
+            if not Compare(top_element, char):
+                return False
+    if len(stack) != 0:
+        return False
+    return True
+def Compare(opening, closing):
+    if opening == '(' and closing == ')':
+        return True
+    if opening == '[' and closing == ']':
+        return True
+    if opening == '{' and closing == '}':
+        return True  
+    return False
+
+print(BalancedBrackets("{rr(4t[.ggg])}"))
+######################################################################

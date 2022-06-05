fcb6bd6fa086a642ed02251dc6f4991121cf2326
Iliya-Tsvibel <45923437+Iliya-Tsvibel@users.noreply.github.com>
2022-06-05 18:48:52 +0300

    Create numbers-to-words.py

---
+++ numbers-to-words.py
@@ -0,0 +1,17 @@
+#1.
+arr = ['zero','one','two','three','four','five','six','seven','eight','nine']
+def number_to_word(n):
+    if(n==0):
+        return ''
+     
+    else:
+        words_from_arr = arr[n%10]
+        res = number_to_word(int(n/10)) + words_from_arr + " "
+        
+    return res
+ 
+n = int(input("Enter number in any range:"))
+print("Entered number is : ", n)
+print("Number in words: ",end="")
+print(number_to_word(n))
+################################
